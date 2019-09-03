50b700a0e3efceaaf542fd88670bd9c6410c9adb
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-03 23:46:09 +0300

    header

--- src/components/Header/Index.vue
+++ src/components/Header/Index.vue
@@ -1,11 +1,17 @@
 <template>
-    $END$
+  <v-app-bar
+    color="deep-purple accent-4"
+    dark
+    app
+  >
+    <v-toolbar-title>Weather</v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <script>
-    export default {
-        name: "Index"
-    }
+export default {
+  name: "Header"
+}
 </script>
 
 <style scoped>
