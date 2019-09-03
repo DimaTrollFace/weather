2397a516246bd99f26ca7a7230e9d7275a6d2593
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-03 23:47:32 +0300

    city card

--- src/pages/Cities/CityCard.vue
+++ src/pages/Cities/CityCard.vue
@@ -1,13 +1,28 @@
 <template>
   <v-card>
-
+    <v-card-title>{{name}}</v-card-title>
+    <v-card-text>I'm card text</v-card-text>
+    <v-card-actions>
+      <v-btn
+        @clik="handlerClickMore"
+      >More</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
-    export default {
-        name: "CityCard"
+export default {
+  name: 'CityCard',
+  props: {
+    id: { type: Number | String, required: true },
+    name: { type: String, required: true }
+  },
+  methods: {
+    handlerClickMore () {
+      this.$emit('click-more', this.id)
     }
+  }
+}
 </script>
 
 <style scoped>
