feeb1281817e9033acdba5cfd44d59e5c66db7af
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-03 23:41:18 +0300

    add vue config

---
+++ src/pages/Cities/CityAdd.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CityAdd"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Cities/CityCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+
+  </v-card>
+</template>
+
+<script>
+    export default {
+        name: "CityCard"
+    }
+</script>
+
+<style scoped>
+
+</style>
