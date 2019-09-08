8e31dae362d1482ef64288e8a2020860c47f0003
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 16:56:09 +0300

    home page

--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -1,11 +1,69 @@
 <template>
-    $END$
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-btn @click="goToCities">Show cities</v-btn>
+        </v-col>
+      </v-row>
+      <div class="current-city-info" v-if="currentCity">
+        <v-row>
+          <v-col>
+            <h1>Weather in your city</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <full-city-info
+              v-bind="currentCity"
+            >
+            </full-city-info>
+          </v-col>
+          <v-col>
+            <forecast-city-info
+              :list="forecastList"
+            >
+            </forecast-city-info>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
 </template>
 
 <script>
-    export default {
-        name: "Index.vue"
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'Home.vue',
+  components: {
+    'forecast-city-info': () => import('@/components/Shared/ForecastCityInfo.vue'),
+    'full-city-info': () => import('@/components/Shared/FullCityInfo.vue'),
+  },
+  data: () => ({
+    forecastList: []
+  }),
+  computed: {
+    ...mapState([
+      'currentCity'
+    ])
+  },
+  methods: {
+    goToCities () {
+      this.$router.push({ name: 'cities' })
+    },
+    ...mapActions([
+      'getForecastByCityID'
+    ])
+  },
+  watch: {
+    currentCity(city) {
+      if (city) {
+        this.getForecastByCityID(city.id)
+          .then((forecast) => {
+            this.forecastList = forecast.list
+          })
+      }
     }
+  }
+}
 </script>
 
 <style scoped>
