0c35263ce89a3424aa02c19ac9829a2d10f02277
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 15:17:48 +0300

    Dialog Confirm

--- src/components/Shared/DialogConfirm.vue
+++ src/components/Shared/DialogConfirm.vue
@@ -1,11 +1,47 @@
 <template>
-    $END$
+  <v-dialog
+    :value="visible"
+  >
+    <v-card>
+      <v-card-title>{{title}}</v-card-title>
+      <v-card-text>{{text}}</v-card-text>
+      <v-card-actions>
+        <div>
+          <v-btn @click="agree">Yes</v-btn>
+          <v-btn @click="disagree">No</v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-    export default {
-        name: "DialogConfirm"
+export default {
+  name: 'DialogConfirm',
+  props: {
+    title: { type: String, default: 'Title' },
+    text: { type: String, default: 'Default confirm text' }
+  },
+  data: () => ({
+    visible: false
+  }),
+  methods: {
+    open () {
+      this.visible = true
+    },
+    close () {
+      this.visible = false
+    },
+    agree () {
+      this.$emit('agree')
+      this.close()
+    },
+    disagree () {
+      this.$emit('disagree')
+      this.close()
     }
+  }
+}
 </script>
 
 <style scoped>
