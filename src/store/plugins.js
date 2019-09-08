60591284f6e54f2564c23e3a312d64b495dcd2c2
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 08:11:46 +0300

    cookies plugin for store

---
+++ src/store/plugins.js
@@ -0,0 +1,11 @@
+function cookies (store) {
+  store.watch((state) => {
+    return state.cityList
+  }, (newVal) => {
+    store._vm.$cookie.setCookie('cityList', newVal)
+  }, {deep: true})
+}
+
+export {
+  cookies
+}
