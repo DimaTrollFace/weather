736b1e73c302f0abfb4925cf7d959940076a3016
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 15:15:59 +0300

    add vue-notification and init filters

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import initFilters from './filters'
 import initPlugins from './plugins'
 import initApp from './initApp'
+import Notifications from 'vue-notification'
 
 Vue.config.productionTip = false
-initPlugins()
+Vue.use(Notifications)
+initPlugins(Vue)
+initFilters(Vue)
 const app = new Vue({
   router,
   store,
