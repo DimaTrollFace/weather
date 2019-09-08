78ece7f0dd9c9b0b93cdf11fd9af72ea892d8494
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 08:09:55 +0300

    main

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import initPlugins from './plugins'
+import initApp from './initApp'
 
 Vue.config.productionTip = false
 initPlugins()
-new Vue({
+const app = new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+})
+initApp(app)
+window.app = app
+app.$mount('#app')
