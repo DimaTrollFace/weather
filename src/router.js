f80c02763e01f524d99b7e888da0422685f50213
dimanevolnichenko@gmail.com <dimanevolnichenko@gmail.com>
2019-09-08 15:34:54 +0300

    delete about

--- src/router.js
+++ src/router.js
@@ -21,14 +21,6 @@ export default new Router({
       path: '/cities/:cityID',
       name: 'city',
       component: () => import(/* webpackChunkName: "about" */ './pages/City/Index.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
